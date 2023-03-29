70fdb217b9fe78a908b321f6e06907c04af6590a
ckomalram <glaw14@gmail.com>
2023-03-28 22:38:26 -0500

    ++ Importando datos + collection

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,6 +1,17 @@
 public class LinqQueries
 {
-    public LinqQueries(){
-        
+    private List<Book> librosCollection = new List<Book>();
+    public LinqQueries()
+    {
+        using (StreamReader reader = new StreamReader("books.json"))
+        {
+            string json = reader.ReadToEnd();
+            this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+    }
+
+    public IEnumerable<Book> TodaLaCollection()
+    {
+        return librosCollection;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+LinqQueries queries = new LinqQueries();
+ImprimirValores(queries.TodaLaCollection());
+void ImprimirValores (IEnumerable<Book> listaLibros){
+    Console.WriteLine("{0 , -60}   {1 , 15}   {2, 15}\n\n", "|| Titulo ||" , "|| #Paginas ||" , "|| FechaPublicado ||");
+
+    foreach (var item in listaLibros)
+    {
+        Console.WriteLine("{0 , -60}   {1 , 15}   {2, 15}\n", item.Title , item.PageCount, item.PublishedDate.ToShortDateString());
+        
+    }
+}
